2caec2dd08febd5ad0ee8218ea6474865805d30e
Nuri Özdaş <32783197+globalaplication@users.noreply.github.com>
2018-03-09 04:02:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -10,12 +10,14 @@ class load(object):
         for test in re:
             self.options.append(test.strip("\n").split(",")[0])
     def get(self, key):
+        if key not in self.options:
+            return "None"
         for options in open(self.file):
             key = options.strip("\n").split(",")[0]
             value = options.strip("\n").split(",")[1]
             self.dict.update({key:value})
         return self.dict[key]
-    def set(self, key, value):
+    def set(self, key, value, string=""):
         key = str(key)
         value = str(value)
         if os.path.exists(self.file) is False:
@@ -23,4 +25,15 @@ class load(object):
         if key not in self.options:
             with open(self.file, "a") as a:
                 a.write(key+","+value+"\n")
+        if key in self.options:
+            for test in self.options:
+                if test == key:
+                    string = string + test + "," + value + "\n"
+                    continue
+                string = string + test + "," + self.get(test) + "\n"
+            with open(self.file, "w") as w:
+                w.write(string)
         return 
+    
+    
+    
